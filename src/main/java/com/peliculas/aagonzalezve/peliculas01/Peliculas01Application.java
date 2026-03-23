ed94116e9f193b699ad4d6f362e091cfdee8a183
Alejandro Gonzalez Vergara <andresg.vergara04@gmail.com>
2026-03-23 12:04:31 -0300

    Primer commit

---
+++ src/main/java/com/peliculas/aagonzalezve/peliculas01/Peliculas01Application.java
@@ -0,0 +1,13 @@
+package com.peliculas.aagonzalezve.peliculas01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Peliculas01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Peliculas01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/peliculas/aagonzalezve/peliculas01/Peliculas01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peliculas.aagonzalezve.peliculas01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Peliculas01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
